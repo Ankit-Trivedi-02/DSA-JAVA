b209e70c2f461527f72e28f08dd68127719c5565
Ankit Trivedi <ankittrivedi0210@gmail.com>
2025-06-09 16:25:10 +0530

    Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte.

---
+++ Basics of Java/Variable decleraion/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String args[]){
+        int age=19;
+        String name="Ankit";
+        char gender='m';
+
+        System.out.println("My name is : " + name + "\nMy age is : " + age + "\nMy gender is : " + gender);
+    }
+}
